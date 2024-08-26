6188efe6788956b503569742bdeeea2820f1be22
choushen <jacob.mckenzie@icloud.com>
2024-08-26 02:12:45 +0100

    DriverFactory Created

---
+++ TrelloTestFramework/TrelloTestFramework/Driver/DriverFactory.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace TrelloTestFramework.TrelloTestFramework.Driver
+{
+
+    public class DriverFactory
+    {
+        private WebDriver _driver;
+        
+        public WebDriver InitDriver() 
+        {
+            string browser = "chrome";
+
+            switch(browser)
+            {
+                case "edge":
+                    _driver = new EdgeDriver();
+                    break;
+                case "firefox":
+                    _driver = new FirefoxDriver();
+                    break;
+                default:
+                    ChromeOptions options = new ChromeOptions();
+                    options.AddArgument("--start-maximized");
+                    _driver = new ChromeDriver(options);
+                    break;
+            }
+
+            return _driver;
+        }
+
+        public void CloseDriver(WebDriver driver)
+        {
+            driver.Quit();
+        }
+
+    }
+
+
+}
