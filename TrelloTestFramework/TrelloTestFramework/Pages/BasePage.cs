98cb72092a5a57ec9efbe2e5f65b37535f918262
choushen <jacob.mckenzie@icloud.com>
2024-08-26 02:06:34 +0100

    Wait added to base page

---
+++ TrelloTestFramework/TrelloTestFramework/Pages/BasePage.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace TrelloTestFramework.TrelloTestFramework.Pages
+{
+
+    public class BasePage
+    {
+        
+        protected IWebElement WaitForElementToBeVisible(WebDriver driver, By Locator, int timeout = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            IWebElement element = wait.Until(driver => {
+                try
+                {
+                    if (driver.FindElement(Locator).Displayed)
+                    {
+                        return (IWebElement?)driver.FindElement(Locator);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (NoSuchElementException)
+                {
+                    return null;
+                }
+            });
+
+            return element;
+        }
+
+    }
+
+
+}
