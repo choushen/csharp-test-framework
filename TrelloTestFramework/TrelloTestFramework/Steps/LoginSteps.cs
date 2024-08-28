cc9867bc30a164fe655d8db826cb167353b709c9
choushen <jacob.mckenzie@icloud.com>
2024-08-28 19:54:03 +0100

    Created steps

--- TrelloTestFramework/TrelloTestFramework/Steps/LoginSteps.cs
+++ TrelloTestFramework/TrelloTestFramework/Steps/LoginSteps.cs
@@ -1,19 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using TechTalk.SpecFlow;
 
 namespace TrelloTestFramework.TrelloTestFramework.Steps
 {
 
+    [Binding]
     public class LoginSteps
     {
         
-        public LoginSteps()
-        {
-            
-        }
+    [Given(@"I navigate to the Trello login page")]
+    public void NavigatedToTrelloLoginPage()
+    {
+    
+    } // end
 
+    // This step definition uses Cucumber Expressions. See https://github.com/gasparnagy/CucumberExpressions.SpecFlow
+    [Then("I should be logged in")]
+    public void ThenIShouldBeLoggedIn()
+    {
+        // Write code here that turns the phrase above into concrete actions
     }
 
+    // This step definition uses Cucumber Expressions. See https://github.com/gasparnagy/CucumberExpressions.SpecFlow
+    [When("I click the login button")]
+    public void WhenIClickTheLoginButton()
+    {
+        // Write code here that turns the phrase above into concrete actions
+    }
 
-}
+    // This step definition uses Cucumber Expressions. See https://github.com/gasparnagy/CucumberExpressions.SpecFlow
+    [When("I enter the username and password")]
+    public void WhenIEnterTheUsernameAndPassword()
+    {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    
+
+    } // end
+} // end
