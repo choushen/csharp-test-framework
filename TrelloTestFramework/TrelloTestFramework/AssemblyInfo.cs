377d0cae6114d1fe3d5ce12e50d74b05fabfa907
choushen <jacob.mckenzie@icloud.com>
2024-08-30 22:02:46 +0100

    Resetting level of paralellism

--- TrelloTestFramework/TrelloTestFramework/AssemblyInfo.cs
+++ TrelloTestFramework/TrelloTestFramework/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using NUnit.Framework;
 [assembly: Parallelizable(ParallelScope.Fixtures)]
-[assembly: LevelOfParallelism(4)]  // Adjust this to the number of desired threads
+[assembly: LevelOfParallelism(3)]  // Adjust this to the number of desired threads
